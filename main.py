bf03be340cc304a3a57a04eadfd2001f9d8057f4
Jingham2510 <jsphingham@gmail.com>
2025-02-25 20:29:09 +0000

    fix inertial issues

--- main.py
+++ main.py
@@ -3,20 +3,22 @@ import time
 import pybullet_data
 import os
 physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
-p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
+
 
 p.setGravity(0,0,-10)
 
 
 #Create visual friction plane - representation of where the plane is 
-fric_plane = p.loadURDF(os.getcwd() + "\models\plane.urdf")
+fric_plane = p.loadURDF(os.getcwd() + "\models\plane.urdf", [0,0,0])
 
-rob = p.loadURDF(os.getcwd() + "\models\IRB_6400.urdf", [0, 0, 0.001])
+rob = p.loadURDF(os.getcwd() + "\models\IRB_6400.urdf", [0, 0, 0.05])
 
+#test = p.loadURDF(os.getcwd() + "/models/test.urdf")
 
 #set the center of mass frame (loadURDF sets base link frame) startPos/Ornp.resetBasePositionAndOrientation(boxId, startPos, startOrientation)
 for i in range (10000):
     p.stepSimulation()
     time.sleep(1./240.)
+
 
 p.disconnect()
